77574e343cb572a2b90365059ec9b53f129bfc90
nixsi <vanyalebedev45@gmail.com>
2026-05-01 10:00:00 +0300

    feat: add command show

--- tiramisu/cli.py
+++ tiramisu/cli.py
@@ -23,12 +23,27 @@ def main():
     run_parser = subparsers.add_parser("run", help="Run a .tira file")
     run_parser.add_argument("file", help="Path to the .tira recipe file")
 
+    # Команда SHOW
+    show_parser = subparsers.add_parser("show", help="Show the raw Python recipe without running")
+    show_parser.add_argument("file", help="Path to the .tira recipe file")
+
     # Считывание аргументов командной строки
     args = parser.parse_args()
 
     # Выполнение логики по команде
     if args.command == "run":
         run_file(args.file)
+
+    elif args.command == "show":
+        try:
+            with open(args.file, 'r', encoding='utf-8') as file:
+                code = file.read()
+
+            print("🐍 Translated Python Recipe 🐍\n")
+            print(translate_tiramisu(code))
+
+        except FileNotFoundError:
+            print(f"❓ [Tiramisu] Error: Could not find recipe '{args.file}'")
 
     else:
         parser.print_help()
