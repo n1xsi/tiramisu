98f8940fca0e1d4ea1efa32f5b9f096d8f36aba1
nixsi <vanyalebedev45@gmail.com>
2026-05-15 10:00:00 +0300

    feat: add repl integration + some code refactor

--- tiramisu/cli.py
+++ tiramisu/cli.py
@@ -1,6 +1,8 @@
 from importlib.metadata import version, PackageNotFoundError
-from .transpiler import run_file, translate_tiramisu
 import argparse
+
+from .transpiler import run_file, translate_tiramisu
+from .repl import start_repl
 
 
 def get_version() -> str:
@@ -28,6 +30,7 @@ def main():
     )
 
     # ------------------ Подменю для команд ------------------
+    # dest="command" означает, что args.command будет содержать имя команды (run, show, repl и т.д.)
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     # Команда RUN (Подготовить и запустить файл)
@@ -38,26 +41,28 @@ def main():
     show_parser = subparsers.add_parser("show", help="Show the raw Python recipe without running")
     show_parser.add_argument("file", help="Path to the .tira recipe file")
 
+    # Команда REPL
+    repl_parser = subparsers.add_parser("repl", help="Start the interactive Tiramisu console")
+
     # Считывание аргументов командной строки
     args = parser.parse_args()
 
-    # Выполнение логики по команде
-    if args.command == "run":
-        run_file(args.file)
-
-    elif args.command == "show":
-        try:
-            with open(args.file, 'r', encoding='utf-8') as file:
-                code = file.read()
-
-            print("🐍 Translated Python Recipe 🐍\n")
-            print(translate_tiramisu(code))
-
-        except FileNotFoundError:
-            print(f"❓ [Tiramisu] Error: Could not find recipe '{args.file}'")
-
-    else:
-        parser.print_help()
+    # ------------------ Логика маршрутизации команд ------------------
+    match args.command:
+        case "run":
+            run_file(args.file)
+        case "show":
+            try:
+                with open(args.file, 'r', encoding='utf-8') as file:
+                    code = file.read()
+                print("🐍 Translated Python Recipe 🐍\n")
+                print(translate_tiramisu(code))
+            except FileNotFoundError:
+                print(f"❓ [Tiramisu] Error: Could not find recipe '{args.file}'")
+        case "repl":
+            start_repl()
+        case _:
+            parser.print_help()
 
 
 if __name__ == "__main__":
